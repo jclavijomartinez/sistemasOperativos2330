55cf137e320d8a278e4edd2e7d54aa05720b7917
Juan Clavijo <jclavijomartinez@gmail.com>
2023-10-01 16:18:47 -0500

    creador funciona

--- proyecto/creadorsparsemat.c
+++ proyecto/creadorsparsemat.c
@@ -1,53 +1,112 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-void generarMatriz(int filas, int columnas, float porcentajeUnos) {
-    int totalElementos = filas * columnas;
-    int totalUnos = totalElementos * porcentajeUnos;
-    int matriz[filas][columnas];
+// Estructura para representar una matriz dispersa
+typedef struct {
+    int **matrix; // Matriz
+    int rows;     // Número de filas
+    int cols;     // Número de columnas
+} SparseMatrix;
 
-    // Inicializar la matriz con ceros
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
-            matriz[i][j] = 0;
+// Función para inicializar una matriz llena de ceros
+SparseMatrix initializeMatrix(int rows, int cols) {
+    SparseMatrix sparseMatrix;
+    sparseMatrix.rows = rows;
+    sparseMatrix.cols = cols;
+
+    // Asignar memoria para la matriz
+    sparseMatrix.matrix = (int **)malloc(rows * sizeof(int *));
+    for (int i = 0; i < rows; i++) {
+        sparseMatrix.matrix[i] = (int *)malloc(cols * sizeof(int));
+        for (int j = 0; j < cols; j++) {
+            sparseMatrix.matrix[i][j] = 0; // Inicializar con ceros
         }
     }
 
-    // Rellenar la matriz con unos
-    for (int k = 0; k < totalUnos; k++) {
-        int filaRandom, columnaRandom;
-        do {
-            filaRandom = rand() % filas;
-            columnaRandom = rand() % columnas;
-        } while (matriz[filaRandom][columnaRandom] == 1);
+    return sparseMatrix;
+}
 
-        matriz[filaRandom][columnaRandom] = 1;
-    }
+// Función para generar valores aleatorios en la matriz
+void generateRandomValues(SparseMatrix *matrix, int nonzeroCount) {
+    srand(time(NULL));
 
-    // Imprimir la matriz
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
-            printf("%d ", matriz[i][j]);
+    for (int i = 1; i <= nonzeroCount; i++) {
+        int row = rand() % matrix->rows;
+        int col = rand() % matrix->cols;
+        if (row != col){
+        matrix->matrix[row][col] = 1; // Valor aleatorio entre 1 y 100
+        } else {
+            matrix->matrix[row][col+1] = 1; 
         }
-        printf("\n");
     }
 }
 
-int main() {
-    int filas, columnas;
-    float porcentaje;
+// Función para escribir la matriz en un archivo
+void writeMatrixToFile(SparseMatrix matrix, const char *filename) {
+    FILE *file = fopen(filename, "w");
 
-    srand(time(NULL)); // Inicializar la semilla para números aleatorios
+    if (file == NULL) {
+        perror("Error al abrir el archivo");
+        exit(EXIT_FAILURE);
+    }
 
-    printf("Introduce el número de filas: ");
-    scanf("%d", &filas);
-    printf("Introduce el número de columnas: ");
-    scanf("%d", &columnas);
-    printf("Introduce el porcentaje de unos (ejemplo: 0.5 para 50%%): ");
-    scanf("%f", &porcentaje);
+    for (int i = 0; i < matrix.rows; i++) {
+        for (int j = 0; j < matrix.cols; j++) {
+            fprintf(file, "%d ", matrix.matrix[i][j]);
+        }
+        fprintf(file, "\n");
+    }
 
-    generarMatriz(filas, columnas, porcentaje);
+    fclose(file);
+}
+
+int aproximacionEntero(float numeroDecimal) {
+    // Extraer la parte decimal
+    float parteDecimal = numeroDecimal - floor(numeroDecimal);
+
+    // Aproximar al entero basado en el criterio
+    if (parteDecimal >= 0.5) {
+        return (int)ceil(numeroDecimal); // Redondear hacia arriba
+    } else {
+        return (int)floor(numeroDecimal); // Redondear hacia abajo
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 5) {
+        printf("Uso: %s <nombre_archivo> <filas> <columnas> <porcentaje>\n", argv[0]);
+        return -1;
+    }
+
+    const char *filename = argv[1];
+    int rows = atoi(argv[2]);
+    int cols = atoi(argv[3]);
+    float porcentaje = atoi(argv[4]);
+    float pospro = porcentaje/100;
+    int nonzeroCount = 0;
+    int zeroCount = aproximacionEntero((rows*cols)*(porcentaje/100));
+
+    nonzeroCount = (rows*cols)-zeroCount;
+
+    if (rows <= 0 || cols <= 0 || nonzeroCount <= 0 || nonzeroCount > rows * cols) {
+        printf("Los parámetros ingresados son incorrectos.\n");
+        return -1;
+    }
+
+    SparseMatrix sparseMatrix = initializeMatrix(rows, cols);
+    generateRandomValues(&sparseMatrix, nonzeroCount);
+    writeMatrixToFile(sparseMatrix, filename);
+    printf("el numero de zerocount es: %d.\n", zeroCount);
+    printf("el numero de nonzero es: %d.\n", nonzeroCount);
+    printf("Matriz generada y escrita en el archivo %s.\n", filename);
+
+    // Liberar memoria de la matriz
+    for (int i = 0; i < sparseMatrix.rows; i++) {
+        free(sparseMatrix.matrix[i]);
+    }
+    free(sparseMatrix.matrix);
 
     return 0;
 }
